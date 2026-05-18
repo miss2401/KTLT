bc79aeae51e9ffddac6d5124d3a1c0c765a0b426
master <nguyenchung2006pn@gmail.com>
2026-05-18 19:57:32 +0700

    Nay co bug

--- Bai4_1.c
+++ Bai4_1.c
@@ -2,12 +2,13 @@
 //MSSV
 //Class
 #include<stdio.h>
+#include<stdlib.h>
 
 int n;
 
-void Tim_Kiem(double arr[], int n, double k){
+void Tim_Kiem(double arr[], int n){
     double k;
-    printf("\n Nhap so can tim: ");
+    printf("\nNhap so can tim: ");
     while(scanf("%lf", &k) != 1 || k < 0){
         printf("k khong thuoc mang");
         break;
@@ -15,22 +16,18 @@ void Tim_Kiem(double arr[], int n, double k){
     for(int i = 0; i < n; i++){
         if(arr[i] == k){
             printf("k nam o vi tri so %d trong mang.", i);
-            return;
         }
     }
     printf("k khong thuoc mang.");
-    return;
 }
-
-void Tim_Kiem_Min(double arr[], int n, double k){
-    
-    for(int i = 0; i < n; i++){
-        if(arr[i] == k){
-            printf("k = 3 nam o vi tri so %d trong mang.", i);
-            return;
+void Tim_Kiem_Min(double arr[], int n){
+    double min = arr[0];
+    for(int i = 1; i < n; i++){
+        if(arr[i] < min){
+            min = arr[i];
         }
     }
-    printf("k khong thuoc mang.");
+    printf("\nSo be nhat cua mang la: %lf", min);
     return;
 }
 
@@ -40,11 +37,15 @@ double *Tao_mang(){
         printf("Nhap lai n: ");
         while(getchar() != '\n');
     }
-    double arr[n];
+    double *arr = malloc(n * sizeof(double));
+    if (arr == NULL) return NULL;
     for(int i = 0; i < n; i++){
         printf("\n");
         printf("Gia tri cua arr[%d] = ", i);
-        scanf("%lf", &arr[i]);    
+        while(!scanf("%lf", &arr[i])){
+            printf("Gia tri khong hop le: ");
+            while(getchar() != "\n");
+        }    
     }
     return arr;
 }
@@ -52,5 +53,6 @@ int main(){
 
     double *arr = Tao_mang();
     Tim_Kiem(arr, n);
-
+    Tim_Kiem_Min(arr, n);
+    printf("\n");
 }
