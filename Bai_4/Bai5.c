a07ef0da8a7bd71cac1097ae51c880f5bb6090bd
master <nguyenchung2006pn@gmail.com>
2026-05-20 07:18:49 +0700

    m

---
+++ Bai_4/Bai5.c
@@ -0,0 +1,15 @@
+//Name
+//MSSV
+//Class
+//5. Thực hiện các phép toán của vecto và ma trận (+, -, tích) qua hàm
+
+#include<stdio.h>
+
+int m, n;
+viet an cuwt 
+
+
+int main(){
+
+    return 0;
+}
